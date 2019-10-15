bfcce0cde533ebb6a3ff673e3646c02ce2f6a959
Adrienne Smith <agsmith046@gmail.com>
2019-10-15 15:57:05 -0500

    changed endpoint for deploy

--- DavidQ-Adrienne-Noteful/noteful/src/config.js
+++ DavidQ-Adrienne-Noteful/noteful/src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'http://localhost:8000/api'
-  // API_ENDPOINT: 'https://dry-ocean-59677.herokuapp.com/api'
+  // API_ENDPOINT: 'http://localhost:8000/api'
+  API_ENDPOINT: 'https://dry-ocean-59677.herokuapp.com/api'
 };
